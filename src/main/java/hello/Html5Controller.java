d88044b5f1783ec058a2ca2389eb5c0caab3fbca
Richard Wyant <rwyant@riseup.net>
2017-09-11 14:21:21 -0500

    fixed radio buttons and added checkboxes

--- src/main/java/hello/Html5Controller.java
+++ src/main/java/hello/Html5Controller.java
@@ -1,3 +1,8 @@
+/*
+08Sep2017 RLW: Replaced @GetMapping and @PostMapping with @RequestMapping
+
+*/
+
 package hello;
 
 import org.springframework.stereotype.Controller;
@@ -12,13 +17,16 @@ import java.util.ArrayList;
 
 @Controller
 public class Html5Controller {
+	
     @GetMapping("/html5") // the last part of the URL
     public String html5Form(Model model) { //String has to be named per template
     	// what model class are we using
     	model.addAttribute("html5", new Html5());
     	// the template that is used to present this to the user
-    	ArrayList<RadioButton> radioButtonsFromController = Html5.findAll();
-    	model.addAttribute("radioButtonsFromController", radioButtonsFromController);
+    	ArrayList<RadioButton> radioButtons = Html5.buildRadioButtons();
+    	ArrayList<CheckBox> checkBoxes = Html5.buildCheckBoxes();
+    	model.addAttribute("radioButtons", radioButtons);
+    	model.addAttribute("checkBoxes", checkBoxes);
     	return "html5";
     }
     
@@ -28,6 +36,5 @@ public class Html5Controller {
     	// the template that is used to present this to the user
         return "html5result";
     }
-    
-    // To do, experiment with @RequestParam
 }
+	
