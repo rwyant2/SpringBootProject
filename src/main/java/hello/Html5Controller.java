17134dfb5d1364dcee50d43431a651a511b1f7c3
Richard Wyant <rwyant@riseup.net>
2017-09-17 23:40:13 -0500

    Added text area, deleted old classes

--- src/main/java/hello/Html5Controller.java
+++ src/main/java/hello/Html5Controller.java
@@ -1,18 +1,10 @@
-/*
-08Sep2017 RLW: Replaced @GetMapping and @PostMapping with @RequestMapping
-
-*/
-
 package hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import java.util.List;
 import java.util.ArrayList;
 
 @Controller
